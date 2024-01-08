479ff8d5d1a6fdbbcb9763653cf92aacb8a7dde0
Bartosz Gołąb <golabWebDev@gmail.com>
2024-01-08 19:17:26 +0100

    TicTacToe start

---
+++ tictactoe/src/App.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <div class="container text-center">
+    <header>
+        <nav>
+      
+        </nav>
+    </header>
+
+    <RouterView />
+  </div>
+</template>
+
+<style >
+ @import './assets/css/main.css';
+</style>
---
+++ tictactoe/src/components/MainContent.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container">
+        <p>TEST</p>
+        <button class="btn btn-danger">XDDD</button>
+    </div>
+  </template>
+  
+  <style scoped>
+  
+  </style>
+  
